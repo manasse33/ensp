6f60fef7bd8f624aa95a27e1bb70c7fb1934469e
Manasse AFOULA <manasseafoula33@gmail.com>
2025-03-29 00:48:01 +0100

    news

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
         const hamburger = document.querySelector('.hamburger');
         hamburger.classList.toggle('active');
     }
+
+        // Fonction pour ajouter ou enlever la classe 'scrolled' à l'header
+        window.addEventListener('scroll', function() {
+            const header = document.querySelector('.header');
+            if (window.scrollY > 0) { // Si l'utilisateur fait défiler la page
+                header.classList.add('scrolled');
+            } else { // Si l'utilisateur est tout en haut de la page
+                header.classList.remove('scrolled');
+            }
+        });
